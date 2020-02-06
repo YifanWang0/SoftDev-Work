4a88f195da19e4cbc75dd3835cc523227d134a44
YifanWang0 <ywang00@stuy.edu>
2020-02-06 03:24:36 -0500

    finished assignment

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,17 +1,55 @@
+//Yifan Wang (Team DropTheBall)
+//SoftDev2 pd1
+//K#04 - I See a Red Door...
+//2020-02-05
+
 var canvas = document.getElementById("slate");
 var ctx = canvas.getContext("2d");
+var mode = 0; //0 for rectangle, 1 for dot
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 80, 80);
-ctx.clearRect(10,10,40,40);
+var clearCanvas = function(e) {
+  console.log(e);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
 
-ctx.font = "100px Verdana";
-ctx.fillStyle = "green";
-ctx.fillText("Hello World", 25, 100, 10);
+var changeRect = function(e) {
+  console.log(e);
+  var displayedState = document.getElementById("mode");
+  displayedState.innerHTML = "Rectangle";
+  mode = 0;
+};
 
-var button = document.getElementById("clear");
-button.addEventListner("click", clearCanvas(canvas, 600, 350));
+var changeDot = function(e) {
+  console.log(e);
+  var displayedState = document.getElementById("mode");
+  displayedState.innerHTML = "Dot";
+  mode = 1;
+};
 
-var clearCanvas = function(name, canvasW, canvasH) {
-  name.clearRect(0,0,canvasW, canvasH);
+var draw = function(e) {
+  console.log(e);
+  let rect = canvas.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+  if (mode == 0){
+    ctx.fillStyle = "red";
+    ctx.fillRect (x, y, 30, 50);
+  }
+  if (mode == 1) {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI, true);
+    ctx.fill();
+  }
 }
+
+var clearButton = document.getElementById("clear");
+clearButton.addEventListener('click', clearCanvas);
+
+var rect = document.getElementById("rectangle");
+rect.addEventListener('click', changeRect);
+
+var dot = document.getElementById("dot");
+dot.addEventListener('click', changeDot);
+
+canvas.addEventListener('click', draw);
